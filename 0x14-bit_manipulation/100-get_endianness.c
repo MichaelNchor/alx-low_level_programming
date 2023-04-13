0a0cb547635f5e6f20b2be74ee06528360f90857
manchor127 <manchor127@gmail.com>
2023-04-13 09:43:07 +0000

    'update'

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-/**
- * Write a function that checks the endianness.
- * Prototype: int get_endianness(void);
- * Returns: 0 if big endian, 1 if little endian
- */
-
 int get_endianness(void)
 {
 	char *yvar;
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,5 @@
 #include "main.h"
 
-/**
- * Write a function that returns the number of bits you would need to flip  
- * to get from one number to another.
- * Prototype: unsigned int flip_bits(unsigned long int n, unsigned long int m);
- * You are not allowed to use the % or / operators
- */
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int between, chk;
